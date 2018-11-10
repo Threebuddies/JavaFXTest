f325f812ec145ebfcff12cb62a25276fec17ac6e
BuildTools <unconfigured@null.spigotmc.org>
2018-11-09 20:27:45 -0500

    Video 3 Complete
    "Anonymous Inner Classes and Lambda Expressions"

--- src/com/gmail/gregrockss/JavaFXTest/Main.java
+++ src/com/gmail/gregrockss/JavaFXTest/Main.java
@@ -8,7 +8,7 @@ import javafx.scene.control.Button;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
-public class Main extends Application implements EventHandler<ActionEvent> {
+public class Main extends Application {
 	
 	Button button;
 	
@@ -21,21 +21,18 @@ public class Main extends Application implements EventHandler<ActionEvent> {
 		primaryStage.setTitle("Title of the Window");
 		button = new Button();
 		button.setText("Click me");
-		button.setOnAction(this);
+		
+		//This class will handle the button events
+		button.setOnAction(e -> {
+			System.out.println("Hey now brown cow");
+			System.out.println("I am a meatball");
+		});
 		
 		StackPane layout = new StackPane();
 		layout.getChildren().add(button);
-		
 		Scene scene = new Scene(layout, 300, 250);
 		primaryStage.setScene(scene);
 		primaryStage.show();
-	}
-	
-	@Override
-	public void handle(ActionEvent event) {
-		if(event.getSource()==button) {
-			System.out.println("You pressed the button");
-		}
 	}
 
 }
