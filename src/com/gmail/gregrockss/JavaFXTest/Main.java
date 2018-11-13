f993de8fbed99dd002c56bc15bd782d0d36ffb25
BuildTools <unconfigured@null.spigotmc.org>
2018-11-12 22:39:38 -0500

    Video 5 Completed
    Creating Alert Boxes

--- src/com/gmail/gregrockss/JavaFXTest/Main.java
+++ src/com/gmail/gregrockss/JavaFXTest/Main.java
@@ -1,19 +1,15 @@
 package com.gmail.gregrockss.JavaFXTest;
 
 import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.control.Label;
 import javafx.scene.layout.StackPane;
-import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class Main extends Application {
 	
 	Stage window;
-	Scene scene1, scene2;
+	Button button;
 	
 	public static void main(String[] args) {
 		launch(args);
@@ -22,28 +18,15 @@ public class Main extends Application {
 	@Override
 	public void start (Stage primaryStage) throws Exception {
 		window = primaryStage;
+		window.setTitle("thenewboston");
 		
-		Label label1 = new Label("Welcome to the first scene!"); 
-		Button button1 = new Button("Go to scene 2");
-		button1.setOnAction(e -> window.setScene(scene2));
+		button = new Button("Click me");
+		button.setOnAction(e -> AlertBox.display("Title of Window", "Wow this alert box is awesome"));
 		
-		//Layout 1 - children are laid out in vertical column
-		VBox layout1 = new VBox(20);
-		layout1.getChildren().addAll(label1,button1);
-		scene1 = new Scene(layout1, 200, 200);
-		
-		//button 2
-		Button button2 = new Button("Go Back to scene 1");
-		button2.setOnAction(e -> window.setScene(scene1));
-		
-		//layout 2
-		StackPane layout2 = new StackPane();
-		layout2.getChildren().add(button2);
-		scene2 = new Scene(layout2, 600, 300);
-		
-		window.setScene(scene1);
-		window.setTitle("Title here");
+		StackPane layout = new StackPane();
+		layout.getChildren().add(button);
+		Scene scene = new Scene(layout, 300, 250);
+		window.setScene(scene);
 		window.show();
-		
 	}
 }
